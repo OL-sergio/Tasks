6be143fbc3106e01e5ade77b5cd823bc0ae8aba1
sergi <a18074@alunos.ipca.pt>
2022-05-17 19:32:49 +0100

    Work Done:
    * Notes:
    Implementing a user Login:
      - Retrofit to access remote API.
      - Listener to prevent an app crash.
    
    * Add items:
    - APIListener.kt
    - HeaderModel.kt
    - PersonService.kt
    - PersonRepository.kt
    - RetrofitClient.kt
    - ValidationListener.kt
    
    * Updated items:
    - LoginViewModel.kt
    - LoginActivity.kt
    - AndroidManifest.xml

---
+++ app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
@@ -0,0 +1,35 @@
+package com.example.tasks.service.repository.remote
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitClient private constructor() {
+
+    companion object {
+
+        private lateinit var retrofit : Retrofit
+        private const val baseURL = "http://devmasterteam.com/CursoAndroidAPI/"
+
+        private fun   getRetrofitInstance() : Retrofit {
+
+            val httpClient = OkHttpClient.Builder()
+            if (!Companion::retrofit.isInitialized){
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+        fun <S> createService(serviceClass: Class<S>): S {
+            return getRetrofitInstance()
+                .create(serviceClass)
+
+        }
+
+    }
+
+}
