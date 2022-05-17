2589f686105872fca5d5a9898b482977b876afbc
sergi <a18074@alunos.ipca.pt>
2022-05-17 22:41:57 +0100

    Work Done:
    * Notes:
    Implementing a user Register:
      - Adding User Registration Code.
    
    * Add items:
    -
    
    * Updated items:
    - LoginViewModel.kt
    - LoginActivity.kt

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -37,4 +37,25 @@ class PersonRepository(val context: Context) {
             }
         })
     }
+
+    fun create (name: String, email: String, password: String, listener: APIListener){
+        val call: Call<HeaderModel> = mRemote.create(name, email, password, true)
+
+        call.enqueue(object : Callback<HeaderModel> {
+            override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS){
+
+                    val validation =  Gson().fromJson(response.errorBody()!!.toString(), String::class.java)
+                    listener.onFailure(validation)
+
+                }else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+            override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
@@ -7,13 +7,21 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface PersonService {
-   // /Authentication/Create
 
     @POST("Authentication/Login")
     @FormUrlEncoded
     fun login(
         @Field("email") email: String,
         @Field("password") password: String
+    ): Call<HeaderModel>
+
+    @POST("Authentication/Create")
+    @FormUrlEncoded
+    fun create(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("receivenews") receivenews: Boolean
     ): Call<HeaderModel>
 
 }
